c97a3718216ce4bbeb53fba7ba23b31d1dcf40ef
Xiaomin Lin <xiaomin940116@hotmail.com>
2016-09-01 21:30:32 -0400

    First version of our Android App

--- app/src/main/java/com/umdasl/robot/robotcontroller/MainActivity.java
+++ app/src/main/java/com/umdasl/robot/robotcontroller/MainActivity.java
@@ -2,13 +2,20 @@ package com.umdasl.robot.robotcontroller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
     Button recordButton;
     boolean recording;
-
+    RelativeLayout leftMotorContainer;
+    ImageView leftMotor;
+    RelativeLayout rightMotorContainer;
+    ImageView rightMotor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +37,26 @@ public class MainActivity extends AppCompatActivity {
                     recordButton.setText("Start Recording");
                     recordButton.setBackgroundColor(getResources().getColor(R.color.Green));
                 }
+            }
+        });
+        leftMotorContainer = (RelativeLayout)findViewById(R.id.leftMotorContainer);
+        leftMotor = (ImageView)findViewById(R.id.leftMotor) ;
+        leftMotorContainer.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                Log.d("robot",Float.toString(event.getY()));
+                leftMotor.setY(event.getY()-leftMotor.getHeight()/2);
+                return false;
+            }
+        });
+        rightMotorContainer = (RelativeLayout)findViewById(R.id.rightMotorContainer);
+        rightMotor = (ImageView)findViewById(R.id.rightMotor) ;
+        rightMotorContainer.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                Log.d("robot",Float.toString(event.getY()));
+                rightMotor.setY(event.getY()-rightMotor.getHeight()/2);
+                return false;
             }
         });
     }
